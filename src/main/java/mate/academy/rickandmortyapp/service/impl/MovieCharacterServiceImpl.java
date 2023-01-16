e8d81f4abe365b8d645158d34c16fa774cbe88c3
galenkoigor <galenkoigor@gmail.com>
2023-01-16 22:26:43 +0100

    Bugs fixed for Docker

--- src/main/java/mate/academy/rickandmortyapp/service/impl/MovieCharacterServiceImpl.java
+++ src/main/java/mate/academy/rickandmortyapp/service/impl/MovieCharacterServiceImpl.java
@@ -8,7 +8,6 @@ import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import lombok.extern.log4j.Log4j2;
-import mate.academy.rickandmortyapp.dto.external.ApiCharacterDto;
 import mate.academy.rickandmortyapp.dto.external.ApiResponseDto;
 import mate.academy.rickandmortyapp.dto.mapper.MovieCharacterMapper;
 import mate.academy.rickandmortyapp.model.MovieCharacter;
